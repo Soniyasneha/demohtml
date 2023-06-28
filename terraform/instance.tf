9c55f129b4ba19fd1ac1629f942bb7868c5f62f7
Soniyasneha <100279856+Soniyasneha@users.noreply.github.com>
2023-06-28 19:31:20 +0530

    Update instance.tf

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "app_and_web_server" {
   ami                         = "ami-05e411cf591b5c9f6"
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.public_subnet.id
-  key_name                    = "keypair"
+  key_name                    = "key pairr"
   associate_public_ip_address = true
 }
 
