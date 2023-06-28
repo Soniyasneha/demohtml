ba94277b44c49553c563a03f4aaad4aad0700e7b
Soniyasneha <100279856+Soniyasneha@users.noreply.github.com>
2023-06-28 18:17:37 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -51,26 +51,6 @@ resource "aws_eip" "example_eip" {
   }
 }
 
-resource "aws_security_group" "example_sg" {
-  name        = "ExampleSecurityGroup"
-  description = "Security group for EC2 instance"
-
-  vpc_id = aws_vpc.example_vpc.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 
 
 
