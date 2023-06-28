2ed7eed8d8891825ba012fd250a0138aea6e9829
Soniyasneha <100279856+Soniyasneha@users.noreply.github.com>
2023-06-28 22:27:51 +0530

    Update bucket.tf

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "<your-bucket-name>"
     key            = "terraform.tfstate"
     region         = "<your-aws-region>"
-    dynamodb_table = "<optional-dynamodb-table-name>"
     encrypt        = true
+    acl            = "private"
   }
 }
