bcd37da786c55e53166cf7a160b36e37f42e5a18
Soniyasneha <100279856+Soniyasneha@users.noreply.github.com>
2023-06-28 18:18:44 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -13,12 +13,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "app_and_web_server" {
-  ami                         = "ami-053b0d53c279acc90"
-  instance_type               = "t2.micro"
-  subnet_id                   = aws_subnet.public_subnet.id
-  key_name                    = "keypair"
-  associate_public_ip_address = true
 
   # User data script to configure the instance
   user_data = <<-EOF
@@ -41,21 +35,4 @@ resource "aws_instance" "app_and_web_server" {
     Name = "AppAndWebServerInstance"
   }
 }
-
-resource "aws_eip" "example_eip" {
-  instance = aws_instance.app_and_web_server.id
-  vpc      = true
-
-  tags = {
-    Name = "ExampleEIP"
-  }
-}
-
-
-
-
-
-
-
-
 
