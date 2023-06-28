1e84bb472f2d4e02e7105be2e27097f273bd8980
Soniyasneha <100279856+Soniyasneha@users.noreply.github.com>
2023-06-28 18:16:57 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -51,12 +51,6 @@ resource "aws_eip" "example_eip" {
   }
 }
 
-resource "aws_route" "public_route" {
-  route_table_id         = aws_route_table.public_route_table.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.example_igw.id
-}
-
 resource "aws_security_group" "example_sg" {
   name        = "ExampleSecurityGroup"
   description = "Security group for EC2 instance"
