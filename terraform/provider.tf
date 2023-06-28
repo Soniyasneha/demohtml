4b3a1716df423429948430ab00c5ff4d7cbb788f
Soniyasneha <100279856+Soniyasneha@users.noreply.github.com>
2023-06-28 18:16:25 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -51,22 +51,6 @@ resource "aws_eip" "example_eip" {
   }
 }
 
-resource "aws_internet_gateway" "example_igw" {
-  vpc_id = aws_vpc.example_vpc.id
-
-  tags = {
-    Name = "ExampleInternetGateway"
-  }
-}
-
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.example_vpc.id
-
-  tags = {
-    Name = "PublicRouteTable"
-  }
-}
-
 resource "aws_route" "public_route" {
   route_table_id         = aws_route_table.public_route_table.id
   destination_cidr_block = "0.0.0.0/0"
