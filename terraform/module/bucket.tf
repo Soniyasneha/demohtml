9d02309362e289ac0ce0018c3f601ff5f12af791
Soniyasneha <100279856+Soniyasneha@users.noreply.github.com>
2023-06-29 07:00:04 +0530

    Update and rename terraform/bucket.tf to terraform/module/bucket.tf

--- terraform/module/bucket.tf
+++ terraform/module/bucket.tf
@@ -1,11 +1,3 @@
-terraform {
-  backend "s3" {
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-  }
-}
-
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_names
   acl    = "private"
