3bed78a0e84fd69e7150092b15e595080058e0a5
Soniyasneha <100279856+Soniyasneha@users.noreply.github.com>
2023-06-29 07:13:56 +0530

    Update bucket.tf

--- terraform/module/bucket.tf
+++ terraform/module/bucket.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
+
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_names
   acl    = "private"
