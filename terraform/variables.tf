ae156eb8954566a33ba86857e8c5bdea81315fd8
Soniyasneha <100279856+Soniyasneha@users.noreply.github.com>
2023-07-03 21:37:38 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,38 +1,49 @@
-variable "vpc_cidr_block" {
-  description = "CIDR block for the VPC"
+variable "instance_name" {
+  description = "Name of the EC2 instance"
   type        = string
-  default     = "10.0.2.0/24"
+  default     = "ExampleAppServerInstance"
 }
 
-variable "vpc_id" {
-  description = "ID of the VPC"
-  type        = string
-  default     = "aws_vpc.example_vpc.id"
+variable "ami_id" {
+  type    = string
+  default = "ami-06b09bfacae1453cb"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+
+variable "key_name" {
+  type    = string
+  default = "key pairr"
+}
+
+variable "associate_public_ip" {
+  type    = bool
+  default = true
+}
+
+variable "vpc_cidr_block" {
+  type    = string
+  default = "10.0.0.0/24"
 }
 
 variable "subnet_cidr_block" {
-  description = "CIDR block for the subnet"
-  type        = string
-  default     = "10.0.2.0/24"
+  type    = string
+  default = "10.0.0.0/24"
 }
 
-variable "subnet_availability_zone" {
-  description = "Availability zone for the subnet"
-  type        = string
-  default     = "us-east-1a"
+variable "availability_zone" {
+  type    = string
+  default = "us-east-1a"
 }
 
-
-variable "route_table_id" {
-  description = "ID of the route table"
+variable "vpc_name" {
+  description = "Name of the VPC"
   type        = string
-  default     = "aws_route_table.public_route_table.id"
-}
-
-variable "destination_cidr_block" {
-  description = "Destination CIDR block for the route"
-  type        = string
-  default     = "0.0.0.0/0"
+  default     = "ExampleVPC"
 }
 
 variable "security_group_name" {
@@ -41,99 +52,16 @@ variable "security_group_name" {
   default     = "ExampleSecurityGroup"
 }
 
-variable "security_group_description" {
-  description = "Description of the security group"
+variable "bucket1_name" {
+  description = "Name of the S3 bucket"
   type        = string
-  default     = "Security group for EC2 instance"
+  default     = "learnterraform1234"
 }
 
-variable "security_group_vpc_id" {
-  description = "ID of the VPC for the security group"
+variable "bucket2_name" {
+  description = "Name of the S3 bucket"
   type        = string
-  default     = "aws_vpc.example_vpc.id"
+  default     = "learnterraform1111"
 }
 
-variable "security_group_ingress_from_port" {
-  description = "Starting port for ingress traffic"
-  type        = number
-  default     = 80
-}
-
-variable "security_group_ingress_to_port" {
-  description = "Ending port for ingress traffic"
-  type        = number
-  default     = 80
-}
-
-variable "security_group_ingress_protocol" {
-  description = "Protocol for ingress traffic"
-  type        = string
-  default     = "tcp"
-}
-
-variable "security_group_ingress_cidr_blocks" {
-  description = "CIDR blocks for ingress traffic"
-  type        = list(string)
-  default     = ["0.0.0.0/0"]
-}
-
-variable "security_group_egress_from_port" {
-  description = "Starting port for egress traffic"
-  type        = number
-  default     = 0
-}
-
-variable "security_group_egress_to_port" {
-  description = "Ending port for egress traffic"
-  type        = number
-  default     = 0
-}
-
-variable "security_group_egress_protocol" {
-  description = "Protocol for egress traffic"
-  type        = string
-  default     = "-1"
-}
-
-variable "security_group_egress_cidr_blocks" {
-  description = "CIDR blocks for egress traffic"
-  type        = list(string)
-  default     = ["0.0.0.0/0"]
-}
-
-variable "ami_id" {
-  description = "AMI ID for the EC2 instance"
-  type        = string
-  default     = "ami-053b0d53c279acc90"
-}
-
-variable "instance_type" {
-  description = "Instance type for the EC2 instance"
-  type        = string
-  default     = "t2.micro"
-}
-
-variable "subnet_id" {
-  description = "ID of the subnet for the EC2 instance"
-  type        = string
-  default     = "aws_subnet.public_subnet.id"
-}
-
-variable "key_name" {
-  description = "Key pair name for SSH access to the EC2 instance"
-  type        = string
-  default     = "keypair"
-}
-
-variable "associate_public_ip" {
-  description = "Flag to associate a public IP address to the EC2 instance"
-  type        = bool
-  default     = true
-}
-
-variable "eip_name" {
-  description = "Name for the Elastic IP (EIP) resource"
-  type        = string
-  default     = "ExampleEIP"
-}
 
