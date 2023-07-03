5e84abfab5ee9eef5ea8a560a3f38cf31290f955
Soniyasneha <100279856+Soniyasneha@users.noreply.github.com>
2023-07-03 10:12:30 +0530

    Update network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,9 +1,9 @@
 resource "aws_vpc" "example_vpc" {
-  cidr_block       = "10.0.2.0/24"
+  cidr_block       = "10.0.0.0/24"
   instance_tenancy = "default"
 
   tags = {
-    Name = "ExampleVPC"
+    Name = "var.vpc_name"
   }
 }
 
@@ -13,11 +13,13 @@ output "vpc_id" {
 
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.example_vpc.id
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = "10.0.0.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
+
+
     Name = "Public Subnet"
   }
 }
@@ -33,13 +35,13 @@ resource "aws_internet_gateway" "example_igw" {
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.example_vpc.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.example_igw.id
+  }
+
   tags = {
     Name = "PublicRouteTable"
   }
 }
 
-resource "aws_route" "public_route" {
-  route_table_id         = aws_route_table.public_route_table.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.example_igw.id
-}
