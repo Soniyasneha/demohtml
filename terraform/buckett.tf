65cf11086ca66d240731b87c5bc89ea280616899
Soniyasneha <100279856+Soniyasneha@users.noreply.github.com>
2023-07-03 10:15:49 +0530

    Update buckett.tf

--- terraform/buckett.tf
+++ terraform/buckett.tf
@@ -1,1 +1,9 @@
+module "bucket1" {
+  source      = "./modules/webserver"
+  bucket_name = var.bucket1_bucket_name
+}
 
+module "bucket2" {
+  source      = "./modules/webserver"
+  bucket_name = var.bucket2_bucket_name
+}
