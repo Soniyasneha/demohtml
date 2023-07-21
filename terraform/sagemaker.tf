5b257d3597e1ba23788977fa152ea45f9a2bd53a
Soniyasneha <100279856+Soniyasneha@users.noreply.github.com>
2023-07-21 14:36:48 +0530

    Update sagemaker.tf

--- terraform/sagemaker.tf
+++ terraform/sagemaker.tf
@@ -2,9 +2,9 @@ resource "aws_sagemaker_endpoint_configuration" "example" {
   name = "example-endpoint-config"
 
   production_variants {
-    variant_name  = "AllTraffic"
+    model_name="mlpractise"
     initial_instance_count = 1
-    instance_type = "ml.m4.xlarge"  # Replace this with your desired instance type
+    instance_type = "ml.t2.medium"  # Replace this with your desired instance type
     initial_variant_weight = 1
   }
 }
