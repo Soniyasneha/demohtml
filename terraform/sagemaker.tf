ee84ef607d8d90320b22f4e732373e18ca2c049a
Soniyasneha <100279856+Soniyasneha@users.noreply.github.com>
2023-07-21 12:47:56 +0530

    Update sagemaker.tf

--- terraform/sagemaker.tf
+++ terraform/sagemaker.tf
@@ -1,6 +1,6 @@
 resource "aws_sagemaker_notebook_instance" "notebook" {
   name          = "my-notebook-instance"
-  role_arn      = arn:aws:iam::933400543978:role/service-role/AmazonSageMaker-ExecutionRole-20230720T082550
+  role_arn      = arn:aws:iam::933400543978:role/sagemaker
   instance_type = "ml.t2.medium"
 
   tags = {
