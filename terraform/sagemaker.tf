50c9f0cd6a2c38cf86f3316d1b3a198ab7e5c460
Soniyasneha <100279856+Soniyasneha@users.noreply.github.com>
2023-07-21 14:31:05 +0530

    Update sagemaker.tf

--- terraform/sagemaker.tf
+++ terraform/sagemaker.tf
@@ -1,9 +1,14 @@
-resource "aws_sagemaker_notebook_instance" "notebook" {
-  name          = "my-notebook-instance"
-  role_arn      = "sagemaker"
-  instance_type = "ml.t2.medium"
+resource "aws_sagemaker_endpoint_configuration" "example" {
+  name = "example-endpoint-config"
 
-  tags = {
-    Name = "sagemaker"
+  production_variants {
+    variant_name  = "AllTraffic"
+    initial_instance_count = 1
+    instance_type = "ml.m4.xlarge"  # Replace this with your desired instance type
+    initial_variant_weight = 1
   }
+}
+resource "aws_sagemaker_endpoint" "example" {
+  name   = "example-endpoint"
+  endpoint_config_name = aws_sagemaker_endpoint_configuration.example.name
 }
